171ce87fdc94e43f6d0d7f0f98d0bb61dd05d94d
alonsoClase <alonbalbuena@educastur.es>
2019-11-18 14:30:56 +0100

    boton burger

---
+++ tareaVelo/js/index.js
@@ -0,0 +1,4 @@
+//hacemos que aparezca el menu cuando le damos al boton BURGER
+document.querySelector(".menu__boton").addEventListener("click", function() {
+  document.querySelector(".menu__contenido").style.opacity = "1";
+});
