5b971cb2d6a94dd76948d3a4324a3ba5da8c5d99
alonsoClase <alonbalbuena@educastur.es>
2019-11-19 13:05:23 +0100

    portada terminada

--- tareaVelo/js/index.js
+++ tareaVelo/js/index.js
@@ -1,4 +1,22 @@
 //hacemos que aparezca el menu cuando le damos al boton BURGER
 document.querySelector(".menu__boton").addEventListener("click", function() {
-  document.querySelector(".menu__contenido").style.opacity = "1";
+  /*cambiamos de estilo*/
+  const menu = document.querySelector(".menu");
+  const contenido = document.querySelector(".contenido");
+
+  if (menu.className != "menu menu--activo") {
+    menu.classList.add("menu--activo");
+    /*desaparecen los botones que se deslizan*/
+    contenido.style.display = "none";
+  } else {
+    /*volvemos a la normalidad*/
+    menu.className = "menu";
+    /*reaparcene los botones que se deslizan*/
+    contenido.style.display = "initial";
+  }
+});
+
+//Si pulsamos en el boton de TIENDA nos redirecciona
+document.querySelector(".boton--tienda").addEventListener("click", function() {
+  window.location.href = "./pagina.html";
 });
