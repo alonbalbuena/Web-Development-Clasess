a0bb8a6e9b1e87afcacd96c8ec58baab67160ee1
alonsoClase <alonbalbuena@educastur.es>
2019-11-20 14:31:09 +0100

    Ajax

--- tareaVelo/js/pagina.js
+++ tareaVelo/js/pagina.js
@@ -4,3 +4,20 @@ document.body.innerHTML = `
 <div class="lista">
 </div>
 `;
+
+function getTareas() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onload = function() {
+    /*pasamos el texto recibido a tipo JSON*/
+    json = JSON.parse(this.responseText);
+    /*el metodo devolvera el array de tareas*/
+    console.log(json.tareas);
+  };
+
+  xhr.open("GET", "./js/lista.json", true);
+
+  xhr.send();
+}
+
+const tareas = getTareas();
