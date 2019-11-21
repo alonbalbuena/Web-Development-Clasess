7cc1973f7a8f2da92e83c5c03bef32e8ba198916
alonsoClase <alonbalbuena@educastur.es>
2019-11-21 13:40:32 +0100

    probando funcionalidad

--- tareaVelo/js/pagina.js
+++ tareaVelo/js/pagina.js
@@ -10,9 +10,11 @@ function getTareas() {
 
   xhr.onload = function() {
     /*pasamos el texto recibido a tipo JSON*/
-    json = JSON.parse(this.responseText);
+    const json = JSON.parse(this.responseText);
     /*el metodo devolvera el array de tareas*/
-    console.log(json.tareas);
+    const tareas = json.tareas.forEach(tarea =>
+      crearTarea(tarea.tarea, tarea.hecha)
+    );
   };
 
   xhr.open("GET", "./js/lista.json", true);
@@ -20,4 +22,12 @@ function getTareas() {
   xhr.send();
 }
 
-const tareas = getTareas();
+function crearTarea(tarea, hecha) {
+  document.querySelector(".lista").innerHTML = `<div class="tarea">
+  <h1>${tarea}</h1>
+  <h1>${hecha}</h1>
+  </div>
+  `;
+}
+
+getTareas();
