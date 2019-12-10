6f7fb1b28de5877d107580647eb18a93f1bea78c
alonsoClase <alonbalbuena@educastur.es>
2019-12-10 13:04:07 +0100

    añadir productos a la tienda

--- proyectoTienda/js/tienda.js
+++ proyectoTienda/js/tienda.js
@@ -2,20 +2,23 @@ const carrito = document.querySelector(".seccion-carrito");
 
 const productos = document.querySelectorAll(".producto");
 
-productos.forEach(producto => {
-  const productoCarrito = document.createElement("p");
-  productoCarrito.className = "producto-carrito";
-
-  productoCarrito.textContent =
-    producto.children[3].firstElementChild.textContent +
-    producto.children[3].lastElementChild.textContent;
-
-  carrito.appendChild(productoCarrito);
-});
-
 //APARICION DEL CARRITO
 document
   .querySelector(".navegacion__carrito")
   .addEventListener("click", function() {
     document.querySelector(".seccion-carrito").style.maxHeight = "200px";
   });
+
+//AGREGAR UN PRODUCTO
+document
+  .querySelectorAll(".boton")
+  .forEach(boton => boton.addEventListener("click", añadirProducto));
+
+function añadirProducto() {
+  const nuevoProducto = document.createElement("div");
+  nuevoProducto.innerHTML = `<img class="producto-carrito__imagen" src = ${this.parentNode.firstElementChild.src} alt = descripcion >
+  <p class="producto-carrito__descripcion">${this.parentNode.lastElementChild.firstElementChild.textContent}</p >`;
+  nuevoProducto.className = "producto-carrito";
+
+  document.querySelector(".seccion-carrito").appendChild(nuevoProducto);
+}
