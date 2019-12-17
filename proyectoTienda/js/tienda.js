4dfaee56389081f1c7d1ddee4d54d67ce70532f0
Ramiro <sinner.roza@gmail.com>
2019-12-17 01:13:34 +0100

    carrito contador y eliminacion

--- proyectoTienda/js/tienda.js
+++ proyectoTienda/js/tienda.js
@@ -10,7 +10,7 @@ document
 
     if (carrito.style.maxHeight == "0px") {
       //aparicion
-      carrito.style.maxHeight = "200px";
+      carrito.style.maxHeight = "500px";
     } else {
       //desaparacion
       carrito.style.maxHeight = "0px";
@@ -22,11 +22,35 @@ document
   .querySelectorAll(".boton")
   .forEach(boton => boton.addEventListener("click", añadirProducto));
 
+/*creamos un contador*/
+var contador = 0;
+var contadorDOM = document.querySelector(".contador-productos");
+
 function añadirProducto() {
   const nuevoProducto = document.createElement("div");
+  nuevoProducto.className = "producto-carrito";
   nuevoProducto.innerHTML = `<img class="producto-carrito__imagen" src = ${this.parentNode.firstElementChild.src} alt = descripcion >
   <p class="producto-carrito__descripcion">${this.parentNode.lastElementChild.firstElementChild.textContent}</p >`;
-  nuevoProducto.className = "producto-carrito";
 
+  /*creo el boton de eliminar*/
+  const eliminar = document.createElement("button");
+  eliminar.textContent = "eliminar";
+  eliminar.addEventListener("click", eliminarProducto);
+  nuevoProducto.appendChild(eliminar);
+
+  /*añadimos todo*/
   document.querySelector(".seccion-carrito").appendChild(nuevoProducto);
+
+  /*sumamos el contador y actualizamos el DOM*/
+  contador += 1;
+  contadorDOM.textContent = contador;
+}
+
+//ELIMINAR UN PRODUCTO
+function eliminarProducto() {
+  this.parentNode.remove();
+
+  /*restamos el contador y actualizamos el DOM*/
+  contador -= 1;
+  contadorDOM.textContent = contador;
 }
