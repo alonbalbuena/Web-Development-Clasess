c3ae7e5798c6fe238f188d1f71e4d80d8328c530
alonso <alonsogb58@educastur.es>
2020-03-02 14:23:14 +0100

    estilos de mapa

--- tareaMapa/Main.js
+++ tareaMapa/Main.js
@@ -1,52 +1,44 @@
-class Icono extends L.Icon {
-  constructor(url) {
-    //llamamos a L.ICON
-    super({
-      iconSize: [25, 25],
-      popupAnchor: [0, -13],
-      //añadimos la caracteristica extra
-      iconUrl: url
-    });
-  }
-}
+//IMPORTAMOS LA CLASE
+import { Icono } from "./Icono.js";
 
+//CREAMOS EL MAPA
+var map = L.map("map").setView([43.522727, -5.662458], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//CREAMOS BOTONES Y SU FUNCIONALIDAD
 document.querySelector(".biblios").addEventListener("click", () => {
   pedirDatos(
     "http://datos.gijon.es/doc/cultura-ocio/bibliotecas.json",
     pintarMarker,
-    new Icono("./book-icon.svg")
+    new Icono("./icons/book-icon.svg")
   );
 });
 document.querySelector(".sanidad").addEventListener("click", () => {
   pedirDatos(
     "http://datos.gijon.es/doc/salud/centros-sanitarios.json",
     pintarMarker,
-    new Icono("./hospital.svg")
+    new Icono("./icons/hospital.svg")
   );
 });
 
-function pedirDatos(url, callback, icono) {
-  var ajax = new XMLHttpRequest();
-  var peticion;
+//PONEMOS UN ICONO POR DEFECTO
+function pedirDatos(url, callback, icono = new Icono("./icons/default.svg")) {
+  let ajax = new XMLHttpRequest();
+  let peticion;
 
   ajax.onload = function() {
     peticion = JSON.parse(this.responseText).directorios.directorio;
-
     //una vez tengamos los datos ejecutamos la funcion que queramos
     //le pasamos el icono (no lo procesamos aqui)
     callback(peticion, icono);
   };
 
-  ajax.open("GET", url, true);
+  ajax.open("GET", url);
 
   ajax.send();
 }
 
-var map = L.map("map").setView([43.522727, -5.662458], 13);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-
-//FUNCIONES
 function pintarMarker(peticion, icono) {
   peticion.forEach(punto => {
     //algunas no tienen coordenadas
@@ -54,9 +46,9 @@ function pintarMarker(peticion, icono) {
       console.log("no tiene coordenadas");
     } else {
       //separamos las coordenadas que las divide un espacio
-      var arrayCoordenadas = punto.localizacion.content.split(" ", 2);
+      let arrayCoordenadas = punto.localizacion.content.split(" ", 2);
 
-      //creamos
+      //creamos el marcador con el icono personalizado que tenemos en un modulo a parte
       const marcador = L.marker([arrayCoordenadas[0], arrayCoordenadas[1]], {
         icon: icono
       });
