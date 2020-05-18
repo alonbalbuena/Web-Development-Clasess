d281f181aa327f4b9b85674454453bfe3784e295
alonBalbuena <alonbalbuena@educastur.es>
2020-05-18 02:01:27 +0200

    paquetes node

--- nodeJS/04-express-server/server.js
+++ nodeJS/04-express-server/server.js
@@ -1,8 +1,39 @@
-const express = require('express');
-const app = new express();
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
 
-app.use(express.static(__dirname + '/public'));
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+const initPage = path.join(__dirname + "/public/home.html");
+
+//API REST
+app.get("/home", (req,res) => {
+  res.sendFile(initPage);
+});
+
+app.get("/usuario/:id", (req,res) => {
+  var conexion = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Hacendado15*",
+    database:"node",
+    port:3306
+  })
+
+  conexion.connect((err)=>{if(err)throw err});
+
+  var query = conexion.query("SELECT * FROM usuarios",(err,data)=>{
+    if(err) throw err;
+    else{
+      if (data.length > 0) res.statusCode(200).json("hay datos");
+      else res.statusCode(400).json("no hay datos");
+    }
+  });
+
+  conexion.end();
+});
 
 const server = app.listen(8888, () => {
-    console.log('Servidor web iniciado');
+  console.log("Servidor web iniciado");
 });
