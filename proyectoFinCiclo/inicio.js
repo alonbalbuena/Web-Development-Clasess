f004f689f67369daeeb99c1bf896220af7ba89fa
alonBalbuena <alonbalbuena@educastur.es>
2020-05-30 01:06:08 +0200

    estilos basicos finalizados

---
+++ proyectoFinCiclo/inicio.js
@@ -0,0 +1,6 @@
+const botonMenu = document.querySelector(".titulo__boton");
+
+botonMenu.addEventListener("click",()=>{
+  document.querySelector(".navegacion").classList.add("navegacion--desplegado");
+})
+
